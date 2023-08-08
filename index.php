7e5f802ecd8ec8929ea0f6488d4480ecd7c7dba8
Jean Ritchy Bastien <bjeanritchy@gmail.com>
2023-08-08 17:17:03 -0400

    les tableaux en php

--- index.php
+++ index.php
@@ -9,6 +9,53 @@
 
 <body>
     <h1>holla</h1>
+    <?php
+
+$recipes = [
+    [
+        'title' => 'Cassoulet',
+        'recipe' => '',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Couscous',
+        'recipe' => '',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => false,
+    ],
+    [
+        'title' => 'Escalope milanaise',
+        'recipe' => '',
+        'author' => 'mathieu.nebra@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Salade Romaine',
+        'recipe' => '',
+        'author' => 'laurene.castor@exemple.com',
+        'is_enabled' => false,
+    ],
+];
+
+
+foreach($recipes as $recepe){
+        if(is_array($recepe)){
+            foreach ($recepe as $key => $value) {
+                echo ("$key $value </br>");
+            }
+        }
+}
+
+?>
+    <h2>aujourdh'ui on est
+        <?php echo(date(('d/m/y h:i:s'))) 
+        
+        ?> </h2>
+
+
+
+
 </body>
 
 </html>
